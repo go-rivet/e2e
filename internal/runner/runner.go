022b8d60eac178a8b42cf2c6f25df62b166a7ed5
Timothy Rule <34501912+trulede@users.noreply.github.com>
2026-05-19 21:04:52 +0200

    Refine runner.

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -2,7 +2,10 @@ package runner
 
 import (
 	"errors"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-rivet/e2e/internal/extensions"
 	"github.com/rogpeppe/go-internal/testscript"
@@ -13,11 +16,31 @@ func Run(testDir string) error {
 		return err
 	}
 
-	t := &cliTest{}
+	var testFiles []string
+	err := filepath.WalkDir(testDir, func(path string, d fs.DirEntry, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
 
+		if !d.IsDir() {
+			name := strings.ToLower(d.Name())
+			if strings.HasSuffix(name, ".txtar") {
+				testFiles = append(testFiles, path)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	if len(testFiles) == 0 {
+		return nil
+	}
+
+	t := &cliTest{}
 	testscript.RunT(t, testscript.Params{
-		Dir:  testDir,
-		Cmds: extensions.Commands(),
+		Files: testFiles,
+		Cmds:  extensions.Commands(),
 		// Setup: func(e *testscript.Env) error {
 		// 	var vars = []string{
 		// 		fmt.Sprintf("VERSION=%s", os.Getenv("VERSION")),
@@ -26,7 +49,6 @@ func Run(testDir string) error {
 		// 	return nil
 		// },
 	})
-
 	if t.failed {
 		return errors.New("one or more E2E test scripts failed")
 	}
